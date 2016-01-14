1458b2576e3a5994d8c84f42f5608260417e40e9
chishi2612 <trungnd1011@gmail.com>
2016-01-14 14:23:24 +0700

    Initial Commit

---
+++ RogoLib/RogoLib.h
@@ -0,0 +1,19 @@
+//
+//  RogoLib.h
+//  RogoLib
+//
+//  Created by Trung Nguyen Duc on 1/14/16.
+//  Copyright © 2016 Robot Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RogoLib.
+FOUNDATION_EXPORT double RogoLibVersionNumber;
+
+//! Project version string for RogoLib.
+FOUNDATION_EXPORT const unsigned char RogoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RogoLib/PublicHeader.h>
+
+
